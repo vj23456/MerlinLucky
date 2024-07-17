d80c74b9ffe2fc299722676b3676ae2f1388e877
vj23456 <53201527+vj23456@users.noreply.github.com>
2024-07-17 23:58:50 +0800

    add

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# MerlinLucky
+# Lucky
+
+## 是什么？
+`运行于ARM KoolCenter软件中心的，一款由Golang写的集合了 端口转发 / DDNS / Web服务 / Stun内网穿透 / 网络唤醒 / 计划任务 / ACME自动证书 / 网络存储 等功能的强大网络工具。`
+
+## 项目地址
+https://github.com/vj23456/MerlinLucky
+
+---
+
+## 机型支持
+
+在asuswrt为基础的固件上，插件目前支持HND和MTK和QCA架构的路由器
---
+++ config.json.js
@@ -0,0 +1,11 @@
+{
+    "author": "vj23456", "Knight", 
+    "description": "端口转发/DDNS/Web服务/Stun内网穿透/网络唤醒/计划任务/ACME自动证书/网络存储", 
+    "home_url": "Module_lucky.asp", 
+    "link": "https://github.com/vj23456/MerlinLucky", 
+    "md5": "44ca9b43b762bb0b24b49d6c40fb044f", 
+    "module": "lucky", 
+    "tags": "工具", 
+    "title": "Lucky", 
+    "version": "1.0"
+}
