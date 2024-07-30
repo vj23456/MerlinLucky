9235bfc9337c164c4d633a53db7da491090a7f73
sadog <sadoneli@gmail.com>
2024-07-30 20:38:25 +0800

    Update config.json.js

--- config.json.js
+++ config.json.js
@@ -1,5 +1,5 @@
 {
-    "author": "vj23456", "kiritoknight", 
+    "author": "vj23456, kiritoknight", 
     "description": "端口转发/DDNS/Web服务/Stun内网穿透/网络唤醒/计划任务/ACME自动证书/网络存储", 
     "home_url": "Module_lucky.asp", 
     "link": "https://github.com/vj23456/MerlinLucky", 
